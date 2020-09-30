800affcd880252ae5deae1616d9528e27a2b6d6d
Jackman3323 <jhughes22@kentdenver.org>
2020-09-30 10:21:28 -0600

    Mutators added to Node.java, toString added as well, need to check .equals to see if I did the correct thing there.

--- Node/src/Node.java
+++ Node/src/Node.java
@@ -43,4 +43,29 @@ public class Node<T>{
     public Node getChild(){
         return this.child;
     }
+
+    //MUTATORS
+    //Mutator--Stored data
+    public void set(T t){
+        this.t = t;
+    }
+    //Mutator--Parent node
+    public void seParent(Node p){
+        this.parent = p;
+    }
+    //Mutator--Child node
+    public void setChild(Node c){
+        this.child  = c;
+    }
+
+    //OVERRIDES OF DEFAULT METHODS
+    //.toString()--returns datatype's toString
+    @Override
+    public String toString(){
+        return this.t.toString();
+    }
+    //.equals()--returns if this datatype equals other node's datatype
+    public boolean equals(Node n){
+        return this.t.equals(n.get());
+    }
 }
